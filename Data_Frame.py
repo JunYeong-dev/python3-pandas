04e963b74d55a75d0bbecf6f71ca04e0e895100c
Jun-cloud <kjy8827@gmail.com>
2020-05-19 21:32:49 +0900

    Series, Data Frame - Slicing Data

--- Data_Frame.py
+++ Data_Frame.py
@@ -42,7 +42,7 @@ frequency_dict2 = {
     'Banana': 5
 }
 
-importance_dict = {
+importance_dict1 = {
     'Apple': 5,
     'Strawberry': 3,
     'Banana': 1
@@ -50,7 +50,7 @@ importance_dict = {
 
 word2 = pd.Series(word_dict2)
 frequency2 = pd.Series(frequency_dict2)
-importance = pd.Series(importance_dict)
+importance = pd.Series(importance_dict1)
 
 summary2 = pd.DataFrame({
     'word': word2,
@@ -62,3 +62,46 @@ score = summary2['frequency'] * summary2['importance']
 summary2['score'] = score
 
 print(summary2)
+
+# 데이터 프레임의 슬라이싱
+
+word_dict3 = {
+    'Apple': '사과',
+    'Strawberry': '딸기',
+    'Banana': '바나나',
+    'Grape': '포도'
+}
+
+frequency_dict3 = {
+    'Apple': 3,
+    'Strawberry': 7,
+    'Banana': 5,
+    'Grape': 1
+}
+
+importance_dict2 = {
+    'Apple': 5,
+    'Strawberry': 3,
+    'Banana': 1,
+    'Grape': 2
+}
+
+word3 = pd.Series(word_dict3)
+frequency3 = pd.Series(frequency_dict3)
+importance2 = pd.Series(importance_dict2)
+
+summary3 = pd.DataFrame({
+    'word': word3,
+    'frequency': frequency3,
+    'importance': importance2
+})
+
+print(summary3)
+
+# 이름을 기준으로 슬라이싱
+# Strawberry에서 Banana까지의 데이터를 가져오는데
+# importance부터 오른쪽에 있는 데이터를 가져오겠다는 의미
+print(summary3.loc['Strawberry':'Banana', 'importance':])
+
+# 인덱스를 기준으로 슬라이싱
+print(summary3.iloc[1:3, 2:])
