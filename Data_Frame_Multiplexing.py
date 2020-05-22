824b82631135116de12a374a66692d051d87bae4
Jun-cloud <kjy8827@gmail.com>
2020-05-22 21:33:43 +0900

    Data Frame - multiplexing example

---
+++ Data_Frame_Multiplexing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+# 데이터 프레임의 다중화
+
+df = pd.DataFrame(
+    np.random.randint(1, 10, (4, 4)),
+    index=[['1차', '1차', '2차', '2차'], ['공격', '수비', '공격', '수비']],
+    columns=['1회', '2회', '3회', '4회']
+)
+
+print(df)
+print(df[["1회", "2회"]].loc["2차"])
